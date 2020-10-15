704061002c70102b62a6a459aa72e9d365fddd9b
kqarlos <kqarlos@hotmail.com>
2020-10-14 19:27:36 -0700

    selects  elements using jquery. Function generates random number for target value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
 $(() => {
-    
-    console.log("ready!");
-    
+
+    //Getting elements
+    var targetEl = document.querySelector('#target');
+    var winsEl = document.querySelector('#wins');
+    var lossesEl = document.querySelector('#losses');
+    var gem1El = document.querySelector('#gem1');
+    var gem2El = document.querySelector('#gem2');
+    var gem3El = document.querySelector('#gem3');
+    var gem4El = document.querySelector('#gem4');
+    var scoreEl = document.querySelector('#score');
+
+    //Sets up the game
+    function setUp() {
+        winsEl.innerHTML = 0;
+        lossesEl.innerHTML = 0;
+        targetEl.innerHTML = generateTarget();
+        scoreEl.innerHTML = 0;
+    }
+
+    //Generates a random target value [19-120]
+    function generateTarget() {
+        return Math.floor((Math.random() * (120 - 19)) + 19);
+    }
+
+    setUp();
+
 });
