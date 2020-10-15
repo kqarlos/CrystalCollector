0932c0bf0257bab6a38d562c098eb796e671d475
kqarlos <kqarlos@hotmail.com>
2020-10-14 19:52:02 -0700

    Updates score with gem value after clicking a gem

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,12 @@ $(() => {
     var targetEl = document.querySelector('#target');
     var winsEl = document.querySelector('#wins');
     var lossesEl = document.querySelector('#losses');
-    var gem1El = document.querySelector('#gem1');
-    var gem2El = document.querySelector('#gem2');
-    var gem3El = document.querySelector('#gem3');
-    var gem4El = document.querySelector('#gem4');
     var scoreEl = document.querySelector('#score');
     var gemValues = {
-        1: 0,
-        2: 0,
-        3: 0,
-        4: 0
+        "gem1": 0,
+        "gem2": 0,
+        "gem3": 0,
+        "gem4": 0
     }
 
     //Sets up the game
@@ -26,7 +22,6 @@ $(() => {
         for (gem in gemValues) {
             gemValues[gem] = generateRandom(1, 12);
         }
-        console.log(gemValues);
     }
 
     //Generates a random target value [19-120]
@@ -37,7 +32,8 @@ $(() => {
     setUp();
 
     $("#gems").on("click", (e) => {
-        console.log(e.target.id);
+        var id = e.target.id;
+        scoreEl.innerHTML = parseInt(scoreEl.innerHTML) + gemValues[id];
     });
 
 });
