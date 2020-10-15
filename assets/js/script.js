8a7c2225d5a302e7e477322092b4b30657266ab1
kqarlos <kqarlos@hotmail.com>
2020-10-14 19:45:20 -0700

    gives values to the gems and adds listener to the gems

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,20 +9,35 @@ $(() => {
     var gem3El = document.querySelector('#gem3');
     var gem4El = document.querySelector('#gem4');
     var scoreEl = document.querySelector('#score');
+    var gemValues = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0
+    }
 
     //Sets up the game
     function setUp() {
         winsEl.innerHTML = 0;
         lossesEl.innerHTML = 0;
-        targetEl.innerHTML = generateTarget();
+        targetEl.innerHTML = generateRandom(19, 120);
         scoreEl.innerHTML = 0;
+        //Gives values to the gems
+        for (gem in gemValues) {
+            gemValues[gem] = generateRandom(1, 12);
+        }
+        console.log(gemValues);
     }
 
     //Generates a random target value [19-120]
-    function generateTarget() {
-        return Math.floor((Math.random() * (120 - 19)) + 19);
+    function generateRandom(min, max) {
+        return Math.floor((Math.random() * (max - min)) + min);
     }
 
     setUp();
+
+    $("#gems").on("click", (e) => {
+        console.log(e.target.id);
+    });
 
 });
